5cb5b6226a90392eaf4cfe3abdcdff7d5c24b1ab
AmjadCodeLab <amjadaliaccen@gmail.com>
2020-11-26 23:22:56 +0530

    commit-26

---
+++ cypress/integration/Spec/test.spec.js
@@ -0,0 +1,18 @@
+/// <reference types="Cypress" />
+describe("Testing Test App",()=>{
+    it("Login Test",()=>{
+        cy.visit("http://eaapp.somee.com/");
+        cy.contains("Login").click();
+        cy.url().should("include","/Account/Login");
+        cy.get('#UserName').type('admin');
+        cy.get('#Password').type('password');
+        cy.get('.btn').click({force: true});
+    })
+    it("Employee List Selecetion",()=>{
+        cy.contains("Employee List").click();
+        // Table -> Rows -> Data
+        cy.get('.table').find('tr')
+        .contains("AutoUser2").parent().contains("Benefits").click();
+        
+    })
+})
